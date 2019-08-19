c4d4a14b2624e3b32055a8fa5ebf532042c2856d
aliza <alizacse@gmail.com>
2019-08-19 20:53:42 +0600

    int to double

--- calculate2.java
+++ calculate2.java
@@ -1,8 +1,11 @@
 public class Calculator{
 	public static void main(String[] args){
 
-	System.out.println(first project in github);	
+	System.out.println(addition(3,4));	
 		
+	}
+  int  addition(int x, int double){
+		return x + y;
 	}
 
        
