1042d94b0705e4368d3e19fe5f8fb46d5bf77a80
aliza <alizacse@gmail.com>
2019-08-19 21:09:19 +0600

    substraction

--- calculate2.java
+++ calculate2.java
@@ -1,11 +1,22 @@
 public class Calculator{
 	public static void main(String[] args){
 
-	System.out.println(addition(3,4));	
+	System.out.println(addition(3,4));
+
+	System.out.println(substraction(3,4));	
 		
 	}
-  int  addition(int x, int double){
+        
+       int  addition(int x, int double)
+        
+        {
 		return x + y;
+	}
+          
+        double subtraction(double x, double y)
+
+        {
+		return x - y;
 	}
 
        
